941414d419e9c127fba9ba2ba69b7c476645fa8b
mizael <mizael0412@gamil.com>
2022-10-13 11:14:29 -0500

    Just Java Final

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.justjava;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import java.text.NumberFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,51 +15,121 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
     int Quan = 0;
+    String whipCream = "No";
+    String chocTop = "No";
+    String theName;
+    int price;
+    boolean checked;
+    boolean cChecked;
+    String orderSummaryD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void addItem(View view){
 
+    public void addItem(View view) {
+            if(Quan<100)
+            {
         Quan++;
-        display (Quan);
+            }
+        displayQuantity(Quan);
     }
 
-    public void minusItem(View view){
-
+    public void minusItem(View view) {
+            if(Quan>0) {
         Quan--;
-        display(Quan);
+            }
+        displayQuantity(Quan);
 
     }
-
 
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-        displayPrice(Quan*5);
-        displayMessage("Total item count: " + Quan + " coffees");
+
+    public String createOrderSummary( int oPrice, int iC, String wC, String cT, String tName) {
+        String orderSummary = ("Name:"+ tName +" \n" + "Quantity: " + iC + "\n" + "Has whipped cream?" + wC + "\n" + "Has chocolate topping?" + cT + "\n" + "Total: $" + oPrice + "\n" + "Thank you!");
+        return orderSummary;
+
+
     }
+    public String addChocTop(View view) {
+        cChecked = ((CheckBox) view).isChecked();
+        switch(view.getId()) {
+            case R.id.choc_Top_Check_Box:
+                if (cChecked)
+                    chocTop = "Yes";
+                if (!cChecked)
+                    chocTop = "No";
+                break;
+
+
+        }
+        return chocTop;
+    }
+
+
+
+
+
+    public String addWhippedCream(View view) {
+        checked = ((CheckBox) view).isChecked();
+        switch(view.getId()) {
+            case R.id.whip_Cream_Check_Box:
+                if (checked)
+                    whipCream = "Yes";
+
+            if (!checked)
+                whipCream = "No";
+                break;
+
+
+        }
+        return whipCream;
+    }
+
+    public void submitOrder(View view) {
+        int price = calculatePrice(Quan);
+        EditText nameField = (EditText) findViewById(R.id.text_input);
+        theName = nameField.getText().toString();
+        orderSummaryD = createOrderSummary(price, Quan, whipCream, chocTop, theName);
+        displayOrderSummary(orderSummaryD);
+        }
+
+
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int Num) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + Num);
     }
+
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+    private void displayOrderSummary(String message) {
+        TextView orderSummary = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummary.setText(message);
     }
 
-    private void displayMessage(String message) {
-        TextView orderSummary = (TextView) findViewById(R.id.price_text_view);
-        orderSummary.setText(message);
+    private int calculatePrice(int quantity) {
+        if (checked && cChecked){
+            price = quantity * 5 + quantity + quantity * 2;
+        }
+        else if (checked) {
+            price = quantity * 5 + quantity;
+        }
+        else if (cChecked) {
+            price = quantity * 5 + quantity * 2;
+        }
+        return price;
+
+
     }
 }
